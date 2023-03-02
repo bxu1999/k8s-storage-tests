38caba16ce3fed24a253db874e76e15dac752390
Bingxue Xu <bxu@us.ibm.com>
2023-03-01 18:05:24 -0800

    updt

--- cleanup.sh
+++ cleanup.sh
@@ -75,7 +75,8 @@ run() {
     echo
   else
     echo
-    echo "oc delete job $(oc get jobs -n $NAMESPACE | awk '{ print $1 }' | grep -i readiness | xargs -I % -n 1 oc delete % -n ${NAMESPACE}) -n $NAMESPACE && \\"
+    oc_delete_tpl="oc get jobs -n $NAMESPACE -o name | grep -i readiness | xargs -I % -n 1 oc delete % -n ${NAMESPACE}"
+    echo "${oc_delete_tpl} && \\"
     echo "oc delete cm $(oc get cm -n $NAMESPACE | awk '{ print $1 }' | grep "consumer-\|producer-") -n $NAMESPACE && \\"
     echo "oc delete pvc $(oc get pvc -n $NAMESPACE | awk '{ print $1 }' | grep -i readiness) -n $NAMESPACE && \\"
     echo "oc delete scc zz-fsgroup-scc"
